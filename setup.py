413890302a8f6b5962dc195db6bdb059a0b34326
Yohan Konan <115831856+fullyohan@users.noreply.github.com>
2026-09-02 03:13:34 +0100

    Modify setup.py with author and description updates
    Updated author name and enhanced package description. Added long description from README.md.

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="psmovebridge",
     version="0.1.0",
-    description="Asynchronous Python client and OpenTrack UDP bridge for PSMoveServiceEx.",
-    author="Your Name",
+    author="Yohan Konan",
+    description="Python client & 6DoF telemetry bridge for PSMoveServiceEx",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fullyohan/psmovebridge",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
         "protobuf>=3.20.0",
     ],
